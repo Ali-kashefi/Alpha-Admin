6d14365a7b24803fd88389abb8b583dc7b9dc8a1
Ali Kashefi <alikashefi274@gmail.com>
2025-09-01 01:21:57 +0330

    feat: UI completion Add avatar Change language button Change theme button Search box Menu icon to open sidebar Search box and buttons and avatar are used from component Styling

--- alpha-admin/src/componenets/Header.jsx
+++ alpha-admin/src/componenets/Header.jsx
@@ -1,32 +1,55 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Button from "./ui/Button";
-import setupThemeSwitcher from "@/utils/theme-switcher";
 import { useThemeStore } from "@/store/themeStore";
+import Image from "next/image";
+import {
+  Bars3CenterLeftIcon,
+  MoonIcon,
+  SunIcon,
+} from "@heroicons/react/24/outline";
+import SearchBox from "./SearchBox";
 function Header() {
-  const [themeManager, setThemeManager] = useState(null);
-  const [isDark, setIsDark] = useState(false);
-  const setTheme = useEffect(() => {
-    const manager = setupThemeSwitcher();
-    setThemeManager(manager);
-    setIsDark(manager.getIsDark());
-  }, []);
-
+  const currentTheme = useThemeStore((state) => state.Theme);
+  const toggleTheme = useThemeStore((state) => state.toggleTheme);
   const handleToggle = () => {
-    if (themeManager) {
-      themeManager.toggleTheme();
-      setIsDark(themeManager.getIsDark());
-    }
+    toggleTheme();
   };
-  console.log(useThemeStore((state) => state.Theme));
-  
+
   return (
     <div>
-      <header className="w-full h-10 ">
-        <ul></ul>
-        <ul>
-          <Button onClick={handleToggle} className="text-white">{isDark?"دارک":"لایت"}</Button>
+      <header className="w-full h-20 bg-primary-50  p-4  flex flex-row justify-between">
+        <ul className="flex flex-row gap-3 flex-nowrap ">
+          <li>
+            <Image
+              src={"/image/download2.png"}
+              width={48}
+              height={60}
+              className="rounded-full"
+              alt="avatra"
+            />
+          </li>
+          <li className="flex justify-center items-center w-12 h-12 border border-primary-300 rounded-full">
+            <Button onClick={handleToggle} className="text-primary-400">
+              {currentTheme === "dark" ? (
+                <SunIcon width={30} hanging={6} />
+              ) : (
+                <MoonIcon width={30} hanging={6} />
+              )}
+            </Button>
+          </li>
+          <li className="flex justify-center items-center w-12 h-12 border  border-primary-300 rounded-full">
+            <Button onClick={handleToggle} className=" text-primary-400">
+              {currentTheme === "dark" ? "En" : "فا"}
+            </Button>
+          </li>
         </ul>
+        <div className="flex flex-row-reverse gap-4">
+          <div className="flex justify-center items-center w-12 h-12 border border-primary-300 rounded-lg">
+            <Bars3CenterLeftIcon className="w-5 h-12" />
+          </div>
+          <SearchBox placeholde="Search or type command..." className="w-96 h-12 rounded-lg border border-primary-300 shadow-md" />
+        </div>
       </header>
     </div>
   );
