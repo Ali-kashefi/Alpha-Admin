7e2e58210989375a7d0f376f405cea20eef8421f
Ali Kashefi <alikashefi274@gmail.com>
2025-09-01 22:56:04 +0330

    feat:Lifting State Up is executed to send values ​​to open the menu. The value is specified in the header and sent to the sidebar by the use state. This is done by Props.

--- alpha-admin/src/app/layout.js
+++ alpha-admin/src/app/layout.js
@@ -3,19 +3,22 @@ import { useThemeStore } from "@/store/themeStore";
 import "./globals.css";
 import Header from "@/componenets/Header";
 import Sidebar from "@/componenets/Sidebar";
+import { useState } from "react";
 
 export default function RootLayout({ children }) {
   const theme = useThemeStore((state) => state.Theme);
+  const [Isopen, setIsopen] = useState(false);
 
   return (
     <html lang="fa" dir="rtl" data-theme={theme} >
+
       <body className="grid grid-rows-10 h-fit grid-cols-9 ">
-        <div className=" col-span-full z-10"><Header /></div>
+        <div className=" col-span-full z-10"><Header setIsopen={setIsopen} /></div>
 
 
 
         <div className="col-span-7 row-span-9">   {children}</div>
-        <div className="row-span-9 col-span-2 ">          <Sidebar /></div>
+        <div className="row-span-9 col-span-2 ">          <Sidebar Isopen={Isopen} /></div>
 
 
 
