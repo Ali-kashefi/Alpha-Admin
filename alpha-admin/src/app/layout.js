ea8d1ede7d0ba9525bca784907a193469a703698
Ali Kashefi <alikashefi274@gmail.com>
2025-09-14 00:39:29 +0330

    refactor: Integrate language change into app provider

--- alpha-admin/src/app/layout.js
+++ alpha-admin/src/app/layout.js
@@ -3,29 +3,27 @@ import { useThemeStore } from "@/store/themeStore";
 import "./globals.css";
 import Header from "@/componenets/Header";
 import Sidebar from "@/componenets/Sidebar";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { AppProvider } from "@/contexts/app-contex";
+import { appWithTranslation } from 'next-i18next';
 
-export default function RootLayout({ children }) {
+import "../core/next-i18next.config";
+
+function RootLayout({ children }) {
   const theme = useThemeStore((state) => state.Theme);
   const [Isopen, setIsopen] = useState(false);
 
-  useEffect(() => {
-    console.log(Isopen);
-  }, [Isopen || setIsopen])
   return (
-    <html lang="fa" dir="rtl" data-theme={theme} >
-
-      <body className="grid grid-rows-10 h-fit grid-cols-9 ">
-        <div className=" col-span-full z-10"><Header setIsopen={setIsopen} /></div>
-
-
-
-        <div className="col-span-7 row-span-9">   {children}</div>
-        <div className="row-span-9 col-span-2 ">          <Sidebar Isopen={Isopen} setIsopen={setIsopen} /></div>
-
-
-
-      </body>
-    </html>
+      <html lang="fa" dir="rtl" data-theme={theme}>
+        <body className="grid grid-rows-10 h-fit grid-cols-9">
+          <AppProvider>
+            <div className=" col-span-full z-10"><Header setIsopen={setIsopen} /></div>
+            <div className="col-span-7 row-span-9"> {children}</div>
+            <div className="row-span-9 col-span-2 "><Sidebar Isopen={Isopen} setIsopen={setIsopen} /></div>
+          </AppProvider>
+        </body>
+      </html>
   );
 }
+
+export default appWithTranslation(RootLayout);
