bc0a9fc7ef7e74c893bfcc30b0e33d11a5107c25
Ali Kashefi <alikashefi274@gmail.com>
2025-09-15 00:45:31 +0330

    feat:Making the Police Holder Component Search bilingual

--- alpha-admin/src/componenets/Header.jsx
+++ alpha-admin/src/componenets/Header.jsx
@@ -11,8 +11,10 @@ import {
 import SearchBox from "./SearchBox";
 import Languageswitcher from "./Languageswitcher";
 import { usePathname } from "next/navigation"; 
+import { useTranslation } from "next-i18next";
 
 function Header({ setIsopen }) {
+  const {t}=useTranslation();
   const currentTheme = useThemeStore((state) => state.Theme);
   const toggleTheme = useThemeStore((state) => state.toggleTheme);
   const handleToggle = () => {
@@ -23,7 +25,7 @@ function Header({ setIsopen }) {
 
   return (
     <div>
-      <header className="w-full h-20 bg-primary-50 p-4 flex flex-row justify-between border-b-1 border-primary-300">
+      <header className="w-full h-20 bg-primary-50 dark:bg-secondary-100 p-4 flex flex-row justify-between border-b-1 outline-0 border-primary-300">
         <ul className="flex flex-row gap-3 flex-nowrap">
           <li>
             <Image
@@ -50,12 +52,12 @@ function Header({ setIsopen }) {
         <div className="flex flex-row-reverse gap-4">
           <div className="flex justify-center items-center w-12 h-12 border border-primary-300 rounded-lg">
             <Button onClick={() => setIsopen((prev) => !prev)}>
-              <Bars3CenterLeftIcon className="w-5 h-12" />
+              <Bars3CenterLeftIcon className="w-5 h-12 text-primary-400" />
             </Button>
           </div>
           <SearchBox
-            placeholde="Search or type command..."
-            className="w-96 h-12 rounded-lg border border-primary-300 shadow-md"
+            placeholde={t("search.Search_title")}
+            className="w-96 h-12 rounded-lg border border-primary-300 shadow-md text-center"
           />
         </div>
       </header>
