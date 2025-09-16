5ca076eace658d833a951a69b393e234365ac2f1
Ali Kashefi <alikashefi274@gmail.com>
2025-09-16 23:26:24 +0330

    feat:Making a dashboard layout with grid

--- alpha-admin/src/app/page.js
+++ alpha-admin/src/app/page.js
@@ -1,16 +1,51 @@
 "use client"
 
-import useGenerateMetadata from "@/hook/useGenerateMetadata";
+import useFormatNumberByLanguage from "@/hook/useFormatNumberByLanguage";
+import GenerateMetadata from "@/utils/GenerateMetadata";
+
 import { useTranslation } from "next-i18next";
+import { FiUsers } from "react-icons/fi";
 
 export default function Home() {
   //Call for bilingualism
   const { t } = useTranslation();
   //Pass the values ​​of the title of this page
-  useGenerateMetadata(t("Metadata.panel"))
+  GenerateMetadata(t("Metadata.panel"))
   return (
-    <div className="h-full w-full">
+    <div className="h-full w-full p ">
+      <h1>{t("home.weeklySales")}</h1>
+      <main>
+        {/*Dashboard*/}
+        <section className=" dark:bg-secondary-100 text-left h-screen p-4 grid grid-cols-4 grid-rows-7 gap-6">
+          <div className="col-span-2 row-span-4 bg-primary-50 rounded-3xl p-4 dark:bg-secondary-200">
+            monyhly
+          </div>
 
+          <div className="col-span-1 flex flex-col  gap-4  items-end row-span-2 bg-primary-50 dark:bg-secondary-200 rounded-3xl p-4">
+            <div className="p-2 rounded-lg bg-primary-100">
+              <FiUsers className="w-7 h-7 text-primary-600" />
+            </div>
+            <p className="text-primary-400"> {t("home.customer")}</p>
+          </div>
+          <div className="col-span-1 flex flex-col gap-4 items-end row-span-2 bg-primary-50 rounded-3xl p-4 dark:bg-secondary-200">
+            <div className="p-2 rounded-lg bg-primary-100">
+              <FiUsers className="w-7 h-7 text-primary-600" />
+            </div>
+            <p className="text-primary-400"> {t("home.customer")}</p>
+            <h2>{useFormatNumberByLanguage(220)}</h2>
+          </div>
+          <div className="col-span-2 row-span-2 bg-primary-50 rounded-3xl p-4 dark:bg-secondary-200">
+            monyhly report
+          </div>
+          <div className="col-span-4 row-span-3 bg-primary-50 rounded-3xl p-4 dark:bg-secondary-200">
+            yearsly report
+          </div>
+
+        </section>
+
+
+
+      </main>
     </div>
   );
 }
