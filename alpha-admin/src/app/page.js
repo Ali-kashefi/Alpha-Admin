fabc68ec8416dee2381ba7967f9a60468e1c411e
Ali Kashefi <alikashefi274@gmail.com>
2025-09-17 01:45:57 +0330

    feat:Data fetching and sending key-query and key-to-hook API for fetching and managing caching and data

--- alpha-admin/src/app/page.js
+++ alpha-admin/src/app/page.js
@@ -1,6 +1,11 @@
 "use client"
 
+import Loading from "@/componenets/ui/Loading";
 import useFormatNumberByLanguage from "@/hook/useFormatNumberByLanguage";
+import useGetData from "@/hook/useGetdata";
+import { getUsers } from "@/services/Services";
+
+
 import GenerateMetadata from "@/utils/GenerateMetadata";
 
 import { useTranslation } from "next-i18next";
@@ -11,9 +16,17 @@ export default function Home() {
   const { t } = useTranslation();
   //Pass the values ​​of the title of this page
   GenerateMetadata(t("Metadata.panel"))
+  const { data, error, isLoading } = useGetData({ queryKey: "uses", queryAPI: getUsers })
+  if (isLoading) {
+    return <Loading />
+  }
+ 
+
+
+
+
   return (
-    <div className="h-full w-full p ">
-      <h1>{t("home.weeklySales")}</h1>
+    <div>
       <main>
         {/*Dashboard*/}
         <section className=" dark:bg-secondary-100 text-left h-screen p-4 grid grid-cols-4 grid-rows-7 gap-6">
@@ -25,14 +38,15 @@ export default function Home() {
             <div className="p-2 rounded-lg bg-primary-100">
               <FiUsers className="w-7 h-7 text-primary-600" />
             </div>
-            <p className="text-primary-400"> {t("home.customer")}</p>
+            <p className="text-primary-400"> {t("home.Total-sales")}</p>
+            <h2 className=" text-3xl">{useFormatNumberByLanguage(nameCount)}</h2>
           </div>
           <div className="col-span-1 flex flex-col gap-4 items-end row-span-2 bg-primary-50 rounded-3xl p-4 dark:bg-secondary-200">
             <div className="p-2 rounded-lg bg-primary-100">
               <FiUsers className="w-7 h-7 text-primary-600" />
             </div>
             <p className="text-primary-400"> {t("home.customer")}</p>
-            <h2>{useFormatNumberByLanguage(220)}</h2>
+            {/* <h2 className=" text-3xl">{useFormatNumberByLanguage(220)}</h2> */}
           </div>
           <div className="col-span-2 row-span-2 bg-primary-50 rounded-3xl p-4 dark:bg-secondary-200">
             monyhly report
