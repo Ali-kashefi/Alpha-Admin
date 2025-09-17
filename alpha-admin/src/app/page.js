1100014658faf2882bea27f9399fe7171b4a2618
Ali Kashefi <alikashefi274@gmail.com>
2025-09-18 01:38:56 +0330

    eat: Add total sales calculation
    This commit introduces a new feature to calculate the total amount of successful invoices.
    
    The code filters the `datainvoices` array to include only invoices with a status of "موفق" and then uses the `reduce` method to sum their `totalAmount`.

--- alpha-admin/src/app/page.js
+++ alpha-admin/src/app/page.js
@@ -1,26 +1,31 @@
 "use client"
-
 import Loading from "@/componenets/ui/Loading";
 import useFormatNumberByLanguage from "@/hook/useFormatNumberByLanguage";
 import useGetData from "@/hook/useGetdata";
-import { getUsers } from "@/services/Services";
-
-
+import { getInvoices, getUsers } from "@/services/Services";
 import GenerateMetadata from "@/utils/GenerateMetadata";
-
 import { useTranslation } from "next-i18next";
+import { FaDollarSign } from "react-icons/fa";
 import { FiUsers } from "react-icons/fi";
-
 export default function Home() {
   //Call for bilingualism
   const { t } = useTranslation();
   //Pass the values ​​of the title of this page
   GenerateMetadata(t("Metadata.panel"))
+  //Get Users
   const { data, error, isLoading } = useGetData({ queryKey: "uses", queryAPI: getUsers })
-  if (isLoading) {
+  //Get Invoices
+  const { data: datainvoices, isLoading: lo } = useGetData({ queryKey: "invoices", queryAPI: getInvoices })
+  if (isLoading || lo) {
     return <Loading />
   }
+  //Calculate the number of users
   const usersCount = data?.length || 0;
+  const total_sales = datainvoices?.filter((invoices) => invoices.status === "موفق").reduce((accumulator, invoice) => {
+    return accumulator + invoice.totalAmount;
+  }, 0);
+
+
 
 
 
@@ -36,17 +41,17 @@ export default function Home() {
 
           <div className="col-span-1 flex flex-col  gap-4  items-end row-span-2 bg-primary-50 dark:bg-secondary-200 rounded-3xl p-4">
             <div className="p-2 rounded-lg bg-primary-100">
-              <FiUsers className="w-7 h-7 text-primary-600" />
+              <FaDollarSign className="w-7 h-7 text-primary-600" />
             </div>
-            <p className="text-primary-400"> {t("home.Total-sales")}</p>
-            <h2 className=" text-3xl">{useFormatNumberByLanguage(usersCount)}</h2>
+            <p className="text-primary-400"> {t("home.total_sales")}</p>
+            <h2 className=" text-3xl">{useFormatNumberByLanguage(total_sales.toLocaleString('en-US'))}</h2>
           </div>
           <div className="col-span-1 flex flex-col gap-4 items-end row-span-2 bg-primary-50 rounded-3xl p-4 dark:bg-secondary-200">
             <div className="p-2 rounded-lg bg-primary-100">
               <FiUsers className="w-7 h-7 text-primary-600" />
             </div>
             <p className="text-primary-400"> {t("home.customer")}</p>
-            {/* <h2 className=" text-3xl">{useFormatNumberByLanguage(220)}</h2> */}
+            <h2 className=" text-3xl">{useFormatNumberByLanguage(usersCount)}</h2>
           </div>
           <div className="col-span-2 row-span-2 bg-primary-50 rounded-3xl p-4 dark:bg-secondary-200">
             monyhly report
