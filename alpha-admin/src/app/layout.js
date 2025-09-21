e95da9292c37d8f4a9a70d3620d596df86e45708
Ali Kashefi <alikashefi274@gmail.com>
2025-09-22 01:46:12 +0330

    feat:Dynamically changing the application language in the body tag and setting it based on the current site language using i18

--- alpha-admin/src/app/layout.js
+++ alpha-admin/src/app/layout.js
@@ -3,26 +3,35 @@ import { useThemeStore } from "@/store/themeStore";
 import "./globals.css";
 import Header from "@/componenets/Header";
 import Sidebar from "@/componenets/Sidebar";
-import { useState } from "react";
+import {  useState } from "react";
 import { AppProvider } from "@/contexts/app-contex";
-import { appWithTranslation } from 'next-i18next';
+import { appWithTranslation, useTranslation } from 'next-i18next';
 
 import "../core/next-i18next.config";
+import clsx from "clsx";
+import Provider from "@/Provider";
 
 function RootLayout({ children }) {
+    const { i18n } = useTranslation();
+
   const theme = useThemeStore((state) => state.Theme);
   const [Isopen, setIsopen] = useState(false);
 
+  
+
+
   return (
-      <html lang="fa" dir="rtl" data-theme={theme}>
-        <body className="grid grid-rows-10 h-fit grid-cols-9">
-          <AppProvider>
-            <div className=" col-span-full z-10"><Header setIsopen={setIsopen} /></div>
-            <div className="col-span-7 row-span-9"> {children}</div>
-            <div className="row-span-9 col-span-2 "><Sidebar Isopen={Isopen} setIsopen={setIsopen} /></div>
-          </AppProvider>
-        </body>
-      </html>
+    <html lang={i18n.language} dir={i18n.dir()} data-theme={theme}>
+      <body className="grid grid-rows-10 h-fit grid-cols-9 dark:bg-secondary-100 bg-primary-100">
+          <Provider>
+        <AppProvider>
+          <div className=" col-span-full z-10"><Header setIsopen={setIsopen} /></div>
+          <div className={clsx(!Isopen ? 'col-span-8' : 'col-span-7', 'row-span-9')}> {children}</div>
+          <div className={clsx('row-span-9', Isopen ? 'col-span-2' : 'col-span-1')} ><Sidebar Isopen={Isopen} setIsopen={setIsopen} /></div>
+        </AppProvider>
+        </Provider>
+      </body>
+    </html>
   );
 }
 
