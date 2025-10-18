8e02db6b65bb9b23718cbc30c61fe7ebb7521229
Ali Kashefi <alikashefi274@gmail.com>
2025-10-18 20:21:54 +0330

    style:Changing fixed colors and adding theme-specific backgrounds

--- alpha-admin/src/componenets/charts/MonthlySalesChart.jsx
+++ alpha-admin/src/componenets/charts/MonthlySalesChart.jsx
@@ -1,7 +1,6 @@
-// MonthlySalesChart.js
-'use client';
+"use client";
 
-import { Bar } from 'react-chartjs-2';
+import { Bar } from "react-chartjs-2";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -10,7 +9,9 @@ import {
   Title,
   Tooltip,
   Legend,
-} from 'chart.js';
+} from "chart.js";
+import { useThemeStore } from "@/store/themeStore";
+import useChartTheme from "@/hook/useChartTheme";
 
 ChartJS.register(
   CategoryScale,
@@ -21,23 +22,42 @@ ChartJS.register(
   Legend
 );
 
-export default function MonthlySalesChart() {
+export default function MonthlySalesChart({ translate }) {
+  
+  
   const data = {
-    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-    datasets: [{
-      label: 'Sales',
-      data: [150, 385, 190, 290, 180, 190, 280, 100, 200, 380, 270, 90],
-      backgroundColor: 'rgba(59, 130, 246, 0.5)',
-      borderColor: 'rgb(59, 130, 246)',
-      borderWidth: 1,
-      barThickness: 16,
-      borderRadius: {
-        topLeft: 8,
-        topRight: 8
-      }
-    }],
+    labels: [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    datasets: [
+      {
+        label: "Sales",
+        data: [150, 385, 190, 290, 180, 190, 280, 100, 200, 380, 270, 90],
+        backgroundColor: "rgba(59, 130, 246, 0.5)",
+        borderColor: "rgb(59, 130, 246)",
+        borderWidth: 1,
+        barThickness: 16,
+        borderRadius: {
+          topLeft: 8,
+          topRight: 8,
+        },
+      },
+    ],
   };
 
+  
+  
   const options = {
     responsive: true,
     maintainAspectRatio: false,
@@ -47,8 +67,8 @@ export default function MonthlySalesChart() {
       },
       title: {
         display: true,
-        text: 'Monthly Sales',
-        color: '#fff',
+        text: `${translate("cahrts.monthly_sales.0.title")}`,
+        color: "dark-mode-text",
         font: {
           size: 20,
         },
@@ -60,14 +80,14 @@ export default function MonthlySalesChart() {
         callbacks: {
           label: (context) => {
             return `Sales: ${context.raw}`;
-          }
+          },
         },
-        backgroundColor: 'rgba(0, 0, 0, 0.7)',
-        titleColor: '#fff',
-        bodyColor: '#fff',
-        borderColor: '#fff',
+        backgroundColor: "rgba(0, 0, 0, 0.7)",
+        titleColor: "#ffffff",
+        bodyColor: "#fff",
+        borderColor: "#fff",
         borderWidth: 1,
-      }
+      },
     },
     scales: {
       x: {
@@ -75,25 +95,25 @@ export default function MonthlySalesChart() {
           display: false,
         },
         ticks: {
-          color: '#888',
+          color: "#888",
         },
       },
       y: {
         grid: {
-          color: 'rgba(255, 255, 255, 0.1)',
+          color: "rgba(255, 255, 255, 0.1)",
         },
         ticks: {
-          color: '#888',
+          color: "#888",
           min: 0,
           max: 400,
-          stepSize: 100 
+          stepSize: 100,
         },
       },
     },
   };
 
   return (
-    <div className="bg-gray-800 p-5 rounded-lg text-white w-full h-full shadow-xl">
+    <div className="dark:bg-gray-800 bg-primary-50 p-5 rounded-lg text-white w-full h-full shadow-xl">
       <Bar data={data} options={options} />
     </div>
   );
