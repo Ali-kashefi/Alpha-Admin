7d4247c055feedb6c49b5f67416325f52a4fecde
Ali Kashefi <alikashefi274@gmail.com>
2025-12-11 22:48:45 +0330

    feat(invoices): Add service to fetch all invoices

--- alpha-admin/src/services/Services.js
+++ alpha-admin/src/services/Services.js
@@ -5,4 +5,9 @@ export function getUsers() {
   return http.get("users.json")
     .then(({ data }) => data);
 }
+export function getInvoices() {
+  
+  return http.get("invoices.json")
+    .then(({ data }) => data);
+}
 
